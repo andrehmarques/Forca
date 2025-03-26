b3e70c5f19a7b50cb111271e2f98642142d9bf28
Andreh Marques <andrehmarquez@gmail.com>
2025-03-26 16:38:16 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite a palavra secreta: ");
+string palavra = Console.ReadLine()!.ToLower();
+
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine()!.ToLower();
+
+bool verificacao = palavra.Contains(letra);
+
+Console.WriteLine($"A letra {letra} existe na palavra secreta => {verificacao}");
